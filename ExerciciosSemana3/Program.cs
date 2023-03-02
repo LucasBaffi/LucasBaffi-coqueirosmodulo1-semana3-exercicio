8a732f2c8abb0b64a49d76a4712516745a38c107
Lucas <lucasrenatobaffi@hotmail.com>
2023-03-01 21:55:49 -0300

    metodo gerar ticket

--- ExerciciosSemana3/Program.cs
+++ ExerciciosSemana3/Program.cs
@@ -1,19 +1,24 @@
 ﻿using parking;
 using parking1;
 
-List<Car> cars = new list<Car>();
+List<Car> cars = new List<Car>();
+
 
 string opcao;
 do
 {
     Console.WriteLine("/n/nOlá bem vindo ao estacionamento Pare aqui , selecione a opcao desejada");
     Console.WriteLine("1 - Cadastrar carro ");
-    Console.WriteLine("2 - MArcar Entrada ");
+    Console.WriteLine("2 - Marcar Entrada ");
     Console.WriteLine("3 - Marcar Saida ");
     Console.WriteLine("4 - Consultar Historico ");
     Console.WriteLine("5 - Sair ");
-    opcao = Console.Read();
+    opcao = Console.ReadLine();
 
+    if (opcao == "1")
+    {
+        CadastrarCarro();
+    }
 } while (opcao != "5");
 
 void CadastrarCarro()
@@ -32,8 +37,36 @@ void CadastrarCarro()
     car.Marca = Console.ReadLine();
 
     cars.Add(car);
-
-
-
-
 }
+
+Car ObterCarro(string placa)
+{
+    foreach (var car in cars)
+    {
+        if (placa == car.Placa)
+        {
+            return car;
+        }
+    }
+    return null;
+}
+
+void GerarTicket(){
+    Console.WriteLine("Qual a placa do veiculo?");
+    string placa = Console.ReadLine();
+    var car = ObterCarro(placa);
+    if(car == null){
+        Console.WriteLine("Carro nao encontrado");  
+        return;
+    }
+    foreach(var ticket in car.Tickets){
+        if(ticket.ativo == true){
+            Console.WriteLine("Veiculo ja esta no estacionamento");  
+        return;
+        }
+
+    }
+}
+
+Ticket ticketNovo = new Ticket();
+ObterCarro.Tickets
